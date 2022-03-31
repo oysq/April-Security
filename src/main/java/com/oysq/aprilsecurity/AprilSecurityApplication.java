20e19172278299a86081d849cc13e154dac61734
oysq <1585228683@qq.com>
2022-04-01 01:09:13 +0800

    创建工程

---
+++ src/main/java/com/oysq/aprilsecurity/AprilSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.oysq.aprilsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprilSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AprilSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oysq/aprilsecurity/AprilSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oysq.aprilsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprilSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
